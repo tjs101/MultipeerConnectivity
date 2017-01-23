19d0a255acb6bc33c8e89471197f82e2737ddd32
quentin <quentin@chenchengs-Mac-mini.local>
2017-01-23 10:15:54 +0800

    新增项目

---
+++ Peer_IOS/Peer_IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Peer_IOS
+//
+//  Created by quentin on 23/01/2017.
+//  Copyright © 2017 Quentin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
